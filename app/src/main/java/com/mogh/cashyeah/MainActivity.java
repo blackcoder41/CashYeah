39d010ce229615fae77efff76e1c4aa8cb408342
vergilsantos <gilsaints88@yahoo.com>
2017-12-02 11:50:58 +0800

    Merge branch 'master' of https://github.com/blackcoder41/CashYeah
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    - added branches classes
    - added transition to line chart

--- app/src/main/java/com/mogh/cashyeah/MainActivity.java
+++ app/src/main/java/com/mogh/cashyeah/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mogh.cashyeah;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,14 +15,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mogh.cashyeah.activities.SpeechScreen;
+import com.mogh.cashyeah.charts.MultiLineChartActivity;
+import com.mogh.cashyeah.models.BranchResponse;
+import com.mogh.cashyeah.services.BranchService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private Button mBtnRequest;
+    private TextView mTxtResponse;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        mBtnRequest = (Button) findViewById(R.id.btnRequest);
+        mTxtResponse = (TextView) findViewById(R.id.txtResponse);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,8 +56,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent speechIntent = new Intent(mContext, SpeechScreen.class);
+                startActivity(speechIntent);
             }
         });
 
@@ -97,5 +129,48 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void getBranches(View view) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.pnb.com.ph/")
+                .build();
+
+        BranchService service = retrofit.create(BranchService.class);
+
+
+
+        Call<List<BranchResponse>> call = service.fetchBranchJSON();
+
+        if (true) return;
+
+
+
+        call.enqueue(new Callback<List<BranchResponse>>() {
+            @Override
+            public void onResponse(Call<List<BranchResponse>> call, Response<List<BranchResponse>> response) {
+
+//                List<BranchResponse> branches = response.body();
+//                String n = branches.get(0).getData().get(0).getName();
+//                mTxtResponse.setText(n);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<BranchResponse>> call, Throwable t) {
+                Log.d("UHACit", "onFailure");
+            }
+        });
+    }
+
+    public void showLineChart(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, MultiLineChartActivity.class);
+        startActivity(intent);
+    }
+
+    public void showSpeech(View view) {
+
     }
 }
---
+++ app/src/main/java/com/mogh/cashyeah/activities/SpeechScreen.java
@@ -0,0 +1,20 @@
+package com.mogh.cashyeah.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mogh.cashyeah.R;
+
+/**
+ * Created by vergilsantos on 02/12/2017.
+ */
+
+public class SpeechScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_screen);
+    }
+}

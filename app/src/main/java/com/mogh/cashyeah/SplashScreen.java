3b6657ac7d5c703ebf6672a6ee5fcf9fb1043e07
blackcoder41 <blackcoder41@gmail.com>
2017-12-03 03:02:27 +0800

    Added Splash

---
+++ app/src/main/java/com/mogh/cashyeah/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.mogh.cashyeah;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, LoginScreen.class);
+        startActivity(intent);
+        finish();
+    }
+}
